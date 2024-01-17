69a536a968284e73fe837ff809ce371af3d51dfd
Aleksei Izmalkin <aai95@yandex.ru>
2024-01-17 14:36:21 +0400

    Remove redundant files from project

--- Picsum/ViewController.swift
+++ Picsum/ViewController.swift
@@ -1,19 +1,10 @@
-//
-//  ViewController.swift
-//  Picsum
-//
-//  Created by Алексей on 17.01.2024.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .systemGreen
     }
-
-
 }
-
